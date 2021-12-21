2fbdacd5c6d80ca14884fcfce62860ddc0a8957e
luisjaar <luisjaarcoding@gmail.com>
2021-12-21 15:20:38 -0600

    first draft of .js to pass first 5 tests, spec.js modified to allow first 5 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+   if(num>=1){ 
+        let answer = "";
+        for (i=0; i<num;i++) {
+            answer += string;
+        }
+        return answer;
 
+   }    else if (num === 0) {
+        return "";
+   }    else {
+        return 'ERROR';
+   }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
