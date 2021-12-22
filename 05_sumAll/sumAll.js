6c756134e521ed8b57602038fb208cd2a5992115
luisjaar <luisjaarcoding@gmail.com>
2021-12-22 15:20:15 -0600

    Updated .js to pass 3rd test by checking entry size first. Updated .spec.js to allow 3rd test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,13 @@ const sumAll = function(entry1,entry2) {
     //initialize return
     let total = 0;
 
+    //logic to switch entries
+    if (entry1 > entry2) {
+        temp = entry1;
+        entry1 = entry2;
+        entry2 = temp;
+    }   
+
     //define loops needed
     let gap = entry2 - entry1;
 
