c49cdca3a936c2d92fe6145dd3122beed060206d
luisjaar <luisjaarcoding@gmail.com>
2021-12-22 15:15:30 -0600

    Completed initial criteria

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(entry1,entry2) {
+    
+    //initialize return
+    let total = 0;
 
+    //define loops needed
+    let gap = entry2 - entry1;
+
+    //loop to add each
+    for (i=0; i<= gap; i++) {
+      if (i === 0){
+        total = entry1;
+        
+      } else {
+        total = total + (entry1 + i);
+        
+      }
+    }
+    return total;
 };
 
 // Do not edit below this line
