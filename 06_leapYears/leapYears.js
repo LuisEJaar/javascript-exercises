debf9f61358b67d9fac406bdb908b5b5191a7051
luisjaar <luisjaarcoding@gmail.com>
2021-12-22 16:28:29 -0600

    Passed first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(entry) {
 
+    if (entry % 4 !== 0) {
+
+        return false;
+
+    } else if (entry % 100 !== 0) {
+        
+        return true;
+
+    } else if (entry % 100 === 0 && entry % 400 === 0) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    } 
 };
 
 // Do not edit below this line
