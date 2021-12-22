db4103aeec59a6fc2de97b3deb10f93fd12aea4f
luisjaar <luisjaarcoding@gmail.com>
2021-12-22 12:07:56 -0600

    Completed first test, updated .spec file to allow second

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,removal) {
+    //Define array
+    let oldArray = array;
 
-};
+    let removedArray = [];
+
+    //Define a counter
+
+    let i = 0;
+
+    //Loop through array
+    oldArray.forEach(function(item) {
+        
+        if (item !== removal) {
+            removedArray[i] = item;
+            i += 1; 
+        } 
+      })
+    
+    return removedArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
