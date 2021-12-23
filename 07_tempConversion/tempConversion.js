48d05200daa44b0e5f793dd6941385283bbd5fef
luisjaar <luisjaarcoding@gmail.com>
2021-12-22 19:07:41 -0600

    Code updated to provide correct conversion, removing .0 float topic for both FtoC and CtoF

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  tempC = (tempF - 32) * 5/9;
+  if (tempC % 1 === 0){
+    return tempC;
+  } else {
+  return tempC.toFixed(1);
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  tempF = (tempC * 9/5) + 32;
+  if (tempF % 1 === 0){
+    return tempF;
+  } else {
+  return tempF.toFixed(1);
+  }
 };
 
 // Do not edit below this line
