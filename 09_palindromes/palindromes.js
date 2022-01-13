099d3111f95ab8ce55084f900f8e89ddf70cd819
luisjaar <luisjaarcoding@gmail.com>
2022-01-13 09:07:27 -0600

    Created palindromes function and passed first test.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (textEntry) {
+    let length = textEntry.length;
+    let stringEnd = length-1;
+    if (length % 2 === 0){ //if its even
+        for (i=0; i <= length/2; i++) {
+            //if end is equal to beginning and so on
+            if (textEntry.charAt(i) === textEntry.charAt(stringEnd-i)){
+                continue;
+            } else {
+                return false;
+            };
+        };
+        return true; 
+    } else {
+        //this one needs to stop before the middle
+        for (i=0; i <= (length/2)-1; i++) {
+            //if end is equal to beginning and so on
+            if (textEntry.charAt(i) === textEntry.charAt(stringEnd-i)){
+                continue;
+            } else {
+                return false;
+            };
+        }; 
+        return true;
+    };
 };
 
 // Do not edit below this line
