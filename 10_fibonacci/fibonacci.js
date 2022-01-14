948b026d3ee7711ddc1f6a7071f3cbacdefae137
luisjaar <luisjaarcoding@gmail.com>
2022-01-13 18:26:33 -0600

    Updated .js to include low numbers, updated .spec.js to allow additional tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,21 @@
-const fibonacci = function(number) {
+const fibonacci = function(entry) {
+    number = Number(entry);
+    console.log(number)
+    console.log(typeof number)
     if (number < 1) {
         return "OOPS";
     };
     let answer = [1,1];
+    if (number <= 2){
+      return answer[number-1];
+    };
     for (i=2; i<=(number);i++) {
       answer[i] = answer[i-1] + answer[i-2];
       console.log(answer[i]);
       if (i === (number-1)) {
-        return answer[i]
+        return answer[i];
       };
     };
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
