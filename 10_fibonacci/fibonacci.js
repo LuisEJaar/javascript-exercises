46e4bd59bb66e52e2a628e83a117a01317299c49
luisjaar <luisjaarcoding@gmail.com>
2022-01-13 18:04:28 -0600

    First draft of .js, passed first test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let answer = [1,1];
+    for (i=2; i<=(number);i++) {
+      answer[i] = answer[i-1] + answer[i-2];
+      console.log(answer[i]);
+      if (i === (number-1)) {
+        return answer[i]
+      };
+    };
 };
 
 // Do not edit below this line
