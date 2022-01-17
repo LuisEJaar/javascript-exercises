0f60a3e5c3201409d2dc7e832c9d0ef6794f6b5a
luisjaar <luisjaarcoding@gmail.com>
2022-01-17 15:34:38 -0600

    Created function to pass test in .js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(object) {
+    //sort from oldest to youngest
+    let sortedObject = object.sort(function(a,b){
+        if (a.yearOfBirth > b.yearOfBirth) {
+            return -1;
+        }   else {
+            return 1;
+        };
+    });
+   
+    let oldestPerson = sortedObject[0];
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
